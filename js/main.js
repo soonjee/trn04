b925c21797a4362adfe71cfc8bfaa6f02261f29a
soonji <youinxxs@naver.com>
2022-08-18 11:48:14 +0900

    0818

--- js/main.js
+++ js/main.js
@@ -1,32 +1,67 @@
 window.addEventListener('DOMContentLoaded', () => {
-
-    const SCE_ELE = document.querySelectorAll('.screvent');
+    // const boxheight = document.querySelector('.hhh').offsetTop;
+    // console.log(boxheight);
 
     window.addEventListener('scroll', () => {
         let sct = window.scrollY;
-        console.log(sct);
+        // console.log(sct);
         sct > 0
             ? document.querySelector('#Header').classList.add('on')
             : document.querySelector('#Header').classList.remove('on')
+
+        sct > 500
+            ? document.querySelector('.to_top').classList.add('on')
+            : document.querySelector('.to_top').classList.remove('on');
+
     });
 
+    const MainSlider = new Swiper('.MainSlider', {
+        loop: true,
+        autoplay: {
+            delay: 2500,
+            disableOnInteraction: false,
+        },
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        },
+    })
+
+    const SCE_ELE = document.querySelectorAll('.active-event')
     window.addEventListener('scroll', () => {
         let sct = window.scrollY;
         SCE_ELE.forEach(el => {
-            sct > el.offsetTop
+            sct > el.offsetTop - 300
                 ? el.classList.add('on')
                 : el.classList.remove('on')
         })
     });
-    // sct > boxheight
-    //     ? document.querySelector('.screvent').classList.add('on')
-    //     : document.querySelector('.screvent').classList.remove('on')
+    // const UL = document.querySelector('nav ul');
+    // const LI = [...UL.children];
+    // console.log(LI);
 
-    const UL = document.querySelector('nav ul');
-    const LI = [...UL.children]; //[1];
-    // const LI = UL.children; [ ] 배열 안에 ul을 늘여놓은것, ... 은 늘여놓았다라는 뜻
-    console.log(LI);
-
-});
+    document.querySelector('.to_top').addEventListener('click', () => {
+        window.scrollTo({ top: 0, behavior: 'smooth' })
+    });
 
 
+    document.querySelector('.mopen').addEventListener('click', (e) => {
+        const TG = e.currentTarget;
+        TG.classList.toggle('on'); //this
+        document.querySelector('#Header').classList.toggle('ox')
+    })
+
+    document.querySelector('#Header').addEventListener('wheel', (e) => {
+        e.preventDefault() // 이벤트 자체를 막음...
+    })
+
+})
+        // });
+        // // sct > boxheight
+        // //     ? document.querySelector('.screvent').classList.add('on')
+        // //     : document.querySelector('.screvent').classList.remove('on')
+
+        // const UL = document.querySelector('nav ul');
+        // const LI = [...UL.children]; //[1];
+        // // const LI = UL.children; [ ] 배열 안에 ul을 늘여놓은것, ... 은 늘여놓았다라는 뜻
+        // console.log(LI);
